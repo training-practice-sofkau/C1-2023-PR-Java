88f4a335114e1fd502641f467622669668783ca2
Juangmcz <juangui-089@hotmail.com>
2023-03-16 20:12:46 -0500

    UpdateBookUseCase was refactored.

--- src/main/java/ec/com/books/sofka/api/usecases/UpdateBookUseCase.java
+++ src/main/java/ec/com/books/sofka/api/usecases/UpdateBookUseCase.java
@@ -1,9 +1,11 @@
 package ec.com.books.sofka.api.usecases;
 
+import ec.com.books.sofka.api.domain.collection.Book;
 import ec.com.books.sofka.api.domain.dto.BookDTO;
 import ec.com.books.sofka.api.repository.IBookRepository;
 import ec.com.books.sofka.api.usecases.interfaces.UpdateBook;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -12,15 +14,15 @@ import reactor.core.publisher.Mono;
 public class UpdateBookUseCase implements UpdateBook {
 
     private final IBookRepository bookRepository;
-    private final SaveBookUseCase saveBookUseCase;
+    private final ModelMapper modelMapper;
 
     public Mono<BookDTO> update(String id, BookDTO bookDTO) {
         return bookRepository.findById(id)
                 .switchIfEmpty(Mono.error(new Throwable("Not found")))
                 .flatMap(book -> {
                     bookDTO.setId(book.getId());
-                    return saveBookUseCase.save(bookDTO);
-                });
+                    return bookRepository.save(modelMapper.map(bookDTO, Book.class));
+                }).map(book -> modelMapper.map(book, BookDTO.class));
     }
-
 }
+
