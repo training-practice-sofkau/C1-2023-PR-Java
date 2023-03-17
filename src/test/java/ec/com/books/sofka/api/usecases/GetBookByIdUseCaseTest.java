167e521f16b136bad40c700484746df49bab9f44
Juangmcz <juangui-089@hotmail.com>
2023-03-16 20:26:05 -0500

    GetBookByIdUseCaseTest was implemented.

---
+++ src/test/java/ec/com/books/sofka/api/usecases/GetBookByIdUseCaseTest.java
@@ -0,0 +1,48 @@
+package ec.com.books.sofka.api.usecases;
+
+import ec.com.books.sofka.api.domain.collection.Book;
+import ec.com.books.sofka.api.repository.IBookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class GetBookByIdUseCaseTest {
+
+    @Mock
+    IBookRepository repoMock;
+
+    ModelMapper modelMapper;
+
+    GetBookByIdUseCase getBookByIdUsecase;
+
+    @BeforeEach
+    void init(){
+        modelMapper = new ModelMapper();
+        getBookByIdUsecase = new GetBookByIdUseCase(repoMock, modelMapper);
+    }
+
+    @Test
+    void getBookById(){
+
+        var book = Mono.just(new Book("1", "title1", 2020));
+
+        Mockito.when(repoMock.findById(ArgumentMatchers.anyString())).thenReturn(book);
+
+        var response = getBookByIdUsecase.apply("bookId");
+
+        StepVerifier.create(response)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        Mockito.verify(repoMock).findById("bookId");
+    }
+
+}
