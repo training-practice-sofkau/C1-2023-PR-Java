b4f58f42d7d1e0eb4e08b5c54fac65323875a4d5
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-17 10:43:55 -0500

    test for the delete usecase

--- src/main/java/ec/com/books/sofka/api/usecases/DeleteBookByIdUseCase.java
+++ src/main/java/ec/com/books/sofka/api/usecases/DeleteBookByIdUseCase.java
@@ -11,18 +11,19 @@ import java.util.function.Function;
 
 @Service
 @AllArgsConstructor
-public class DeleteBookByIdUseCase implements Function<String, Mono<String>> {
+public class DeleteBookByIdUseCase implements Function<String, Mono<Void>> {
 
     private final IBookRepository bookRepository;
     private final ModelMapper mapper;
 
     @Override
-    public Mono<String> apply(String ID) {
+    public Mono<Void> apply(String ID) {
         return this.bookRepository
                 .findById(ID)
                 .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NOT_FOUND.toString())))
-                .flatMap(book -> this.bookRepository.deleteById(book.getId())
-                .then(Mono.just(ID))).switchIfEmpty(Mono.empty());
+                .flatMap(book -> this.bookRepository.deleteById(book.getId()));
+                //.then(Mono.just(ID))).switchIfEmpty(Mono.empty());
+
     }
 
 }
--- src/test/java/ec/com/books/sofka/api/usecases/DeleteBookByIdUseCaseTest.java
+++ src/test/java/ec/com/books/sofka/api/usecases/DeleteBookByIdUseCaseTest.java
@@ -1,17 +1,50 @@
 package ec.com.books.sofka.api.usecases;
 
+import ec.com.books.sofka.api.domain.collection.Book;
 import ec.com.books.sofka.api.repository.IBookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteBookByIdUseCaseTest {
-
+    @Mock
     IBookRepository repoMock;
     ModelMapper mapper;
-    DeleteBookByIdUseCase deleteBookByIdUseCase;
+    DeleteBookByIdUseCase service;
 
+    @BeforeEach
+    void init(){
+        mapper = new ModelMapper();
+        service = new DeleteBookByIdUseCase(repoMock, mapper);
+    }
+
+    @Test
+    @DisplayName("deleteByID_Success")
+    void deleteByIdSuccess() {
+        //Build the scenario you need
+        var monoBook = Mono.just(new Book("1", "title1", 2020));
+
+        Mockito.when(repoMock.findById(ArgumentMatchers.anyString())).thenReturn(monoBook);
+        Mockito.when(repoMock.deleteById(ArgumentMatchers.anyString())).thenReturn(Mono.empty());
+
+        var response = service.apply(ArgumentMatchers.anyString());
+
+        StepVerifier.create(response)
+                .expectNextCount(0)
+                .verifyComplete();
+
+        Mockito.verify(repoMock).findById(ArgumentMatchers.anyString());
+        Mockito.verify(repoMock).deleteById(ArgumentMatchers.anyString());
+    }
 }
